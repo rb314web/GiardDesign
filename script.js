3e055edfd95a49c15b7b401bc7ecb4d780d8c9f4
Radosław <brzezinskiradek@gmail.com>
2023-08-19 23:15:27 +0200

    add realize

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 const dropdownElementList = document.querySelectorAll('.dropdown-toggle')
 const dropdownList = [...dropdownElementList].map(dropdownToggleEl => new bootstrap.Dropdown(dropdownToggleEl))
+
+
+var msnry = new Masonry( '.grid', {
+    columnWidth: 10,
+    itemSelector: '.grid-item'
+  });
