4f02e738641a52125693fee1be1caa11c177498d
Radosław <brzezinskiradek@gmail.com>
2023-08-20 15:09:09 +0200

    add macy

--- script.js
+++ script.js
@@ -6,3 +6,18 @@ var msnry = new Masonry( '.grid', {
     columnWidth: 10,
     itemSelector: '.grid-item'
   });
+
+
+  var macy = Macy({
+    container: '#macy-container',
+    trueOrder: false,
+    waitForImages: false,
+    margin: 24,
+    columns: 6,
+    breakAt: {
+        1200: 5,
+        940: 3,
+        520: 2,
+        400: 1
+    }
+});
